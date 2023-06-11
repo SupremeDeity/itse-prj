130b4e3592b3eeffd3f663e8a7daefb4a007707b
SupremeDeity <29257166+SupremeDeity@users.noreply.github.com>
2023-06-11 18:00:28 +0500

    style changing to main

--- pages/main.tsx
+++ pages/main.tsx
@@ -16,7 +16,7 @@ export default function Main() {
     auth.logOut();
   };
   return (
-    <div className={kanit.className}>
+    <div id="mainDiv" className={kanit.className}>
       <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
